ef2e3890135bab664db85d47c6b8fc65937a5a38
yang.xu <yang.xu@shuyun.com>
2018-05-30 10:05:29 +0800

    修复bug

--- src/main.js
+++ src/main.js
@@ -26,37 +26,56 @@ const router = new VueRouter({
     routes
 });
 
-const pathAuth = ['/menu', '/mondul', '/web'];
+const  localUser = {
+    "id": 1,
+    "username": "huoqiuzhongxin",
+    "name": "管理员",
+    "phone": "18712341234",
+    "email": "admin@huoqiu.com",
+    "password": "CAE6704A442B9797040F573AC03B41A3",
+    "salt": 4441,
+    "roleId": -1,
+    "picUrl": "/images/user.jpg",
+    "intro": "柴静，女，出生于山西临汾，毕业于中国传媒大学，记者、主持人。",
+    "creareDate": 1521615603000
+};
+
+
+
+
 router.beforeEach((to, from, next) => {
     //NProgress.start();
     if (to.path == '/login') {
         localStorage.removeItem('user');
-    }
-    let user = JSON.parse(localStorage.getItem('user'));
-
-    if (user && user.roleId > 1 && pathAuth.includes(to.path)) {
-        next({path: '/'});
     } else {
-        if (!user && to.path != '/login' && to.path != '/regist') {
-            next({path: '/login'})
-        } else {
-            next()
-        }
+        localStorage.setItem('user', JSON.stringify(localUser));
     }
+    // let user = JSON.parse(localStorage.getItem('user'));
+    //
+    // if (user && user.roleId > 1 && pathAuth.includes(to.path)) {
+    //     next({path: '/'});
+    // } else {
+    //     if (!user && to.path != '/login' && to.path != '/regist') {
+    //         next({path: '/login'})
+    //     } else {
+    //         next()
+    //     }
+    // }
 
-    if (to.path != '/login' && to.path != '/regist') {
-        getSessionUser().then(res => {
-            if (!res.data.data) {
-                localStorage.removeItem('user');
-                next({path: '/login'});
-            } else {
-                if (!localStorage.getItem('user')) {
-                    localStorage.setItem('user', JSON.stringify(res.data.data));
-                }
 
-            }
-        });
-    }
+//  if (to.path != '/login' && to.path != '/regist') {
+//      getSessionUser().then(res => {
+//          if (!res.data.data) {
+//              localStorage.removeItem('user');
+//              next({path: '/login'});
+//          } else {
+//              if (!localStorage.getItem('user')) {
+//                  localStorage.setItem('user', JSON.stringify(res.data.data));
+//              }
+//
+//          }
+//      });
+//  }
 });
 //router.afterEach(transition => {
 //NProgress.done();
